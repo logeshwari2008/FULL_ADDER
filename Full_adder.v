7cb82c2ad8e0a4ecc88e91fff01f98d24e6cf055
logeshwari2008 <nlogeshwari59@gmail.com>
2025-12-14 08:40:59 +0530

    Add files via upload

---
+++ Full_adder.v
@@ -0,0 +1,12 @@
+module Full_adder (
+    input  wire a, b, cin,   
+    output wire sum, carry   
+);
+
+   
+    assign sum   = a ^ b ^ cin;                  
+    assign carry = (a & b) | (b & cin) | (a & cin); 
+
+endmodule
+
+
