a202f053dc5fc39a4ee06b266d9e53a126ebba9a
logeshwari2008 <nlogeshwari59@gmail.com>
2025-12-17 14:48:25 +0530

    Add files via upload

---
+++ Full_sub.v
@@ -0,0 +1,8 @@
+module Full_sub (
+    input  wire a, b, bin,      
+    output wire diff, borrow    
+);
+    assign diff   = a ^ b ^ bin;                  
+    assign borrow = (~a & b) | (~(a ^ b) & bin);  
+
+endmodule
